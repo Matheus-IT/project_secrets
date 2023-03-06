dbf70ec140981a4d4665c979f48dff9ffc2dce2c
matheus costa <matheuscosta.tech@gmail.com>
2023-03-06 20:21:02 -0300

    Implementing level 1 auth

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -10,6 +11,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
 	extended: true,
 }));
+
+const database = 'userDB';
+mongoose.connect(`mongodb://localhost:27017/${database}`);
+
+
+const userSchema = {
+	email: String,
+	password: String,
+};
+const User = mongoose.model('User', userSchema);
+
 
 app.listen(3000, function() {
 	console.log('Server started on port 3000');
@@ -25,4 +37,26 @@ app.get('/login', function(req, res) {
 
 app.get('/register', function(req, res) {
 	res.render('register');
+});
+
+app.post('/register', function(req, res) {
+	const newUser = new User({
+		email: req.body.username,
+		password: req.body.password,
+	});
+	newUser.save().then(() => {
+		res.render('secrets');
+	}).catch(error => console.log(error));
+});
+
+app.post('/login', function(req, res) {
+	const { username, password } = req.body;
+
+	User.findOne({ email: username }).exec().then((user) => {
+		if (user.password === password) {
+			res.render('secrets');
+		} else {
+			console.log('password din\'t match!');
+		}
+	});
 });
